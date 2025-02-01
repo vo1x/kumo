a733fbb91781c88bc47655122f9bffa80f878eb7
volx <97158976+vo1x@users.noreply.github.com>
2025-02-01 22:03:33 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
